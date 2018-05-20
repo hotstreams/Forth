e23ee922d87953efd6f5bc3809ca214a0b2d8746
Eddie S <cloud9nine@yandex.ru>
2018-05-21 02:58:14 +0300

    up

--- README.md
+++ README.md
@@ -20,26 +20,87 @@
 
 * "number"  (   -- n ) ; read int
 
-* "mem"     (   -- mem_address )
+* "mem_addr"     (   -- mem_address )
 
 * "!"       ( data address -- ) ; write data to address
 
 * "@"       ( address -- data ) ; read data from address
 
-* "quit" ; exits
+* "quit" ; rip
 
 * ":" ; start defining word
 
 * ";" ; end of definition IMMEDIATE
 
-* "lit" "n" ; push n   
+* "lit" "n" ; push n 
+
+* "word"    ( addr -- length ) ; read word from stdin 
+
+* "count"   ( str_address -- length )
+
+* ">r"      ( data --  ) ; from data stack to rstack
+
+* "r>"      (  -- data ) ; from rstack to data stack
+
+* "r@"      (  -- data ) ; non destructive
+
+* "c!"	    ( char address -- ) write char
+
+* "c@"	    ( addr -- char) read char
+
+* "branch addr"  ; jmp to xt is stored by addr
+* "branch0 addr" ; jmp if tos == 0
+
+* "printc"  ( addr count -- ) ; print count chars from addr
+
+* "prints"  ( addr -- ) ; print string from addr
+
+* "syscall" ( call_num a1 a2 a3 a4 a5 a6 -- new_rax new_rdx )
+
+* "," ( a -- ) ; add a to defining word
+
+* "c," ( a -- ) ; add byte to defining word
+
+* "last_word_addr" (  -- last_word )
+
+* "word_buffer_addr" (  -- word_buffer )
+
+* "state_addr" (  -- state )
+
+* "here_addr" (  -- [here] )
+
+* "find_word" ( word_addr -- w_word )
+
+* "cfa"	      ( w_word -- ex_token )
+
+* "'"         (  -- ex_token ) ; read word and place it's xt
+
+* "execute"   ( ex_token --  ) ; execute word
+
+* "IMMEDIATE" ; set last word's flag = 1
+
+* "push_xt_branch" (  -- xt_branch )
+
+* "push_xt_branch0" ( -- xt_branch0 )
+
+* "if else then" ; for compiler mode
+
+* "print_colon word" ; print ex_tokens for word
 
 Arithmetic:
 * "+"       ( a b -- [a + b] )
 * "-"       ( a b -- [a - b] )
 * "*"       ( a b -- [a * b] )
 * "/"       ( a b -- [a / b] )
+* "<"	    ( a b -- [a < b] )
+* "%"	    ( a b -- [a mod b] )
 
 Logic:
-* "land"    ( a b -- a && b )
-* "not"     ( a -- !a )
+* "land"    ( a b -- [a && b] )
+* "lor"     ( a b -- [a || b] )
+* "not"     ( a -- [!a] )
+* "="	    ( a b -- [a == b])
+
+Bit:
+* "and"     ( a b -- [a & b] )
+* "or"      ( a b -- [a | b] )
--- src/forth.asm
+++ src/forth.asm
@@ -7,48 +7,40 @@
 
 %include "words.inc"
 
-
 section .data
-last_word: dq link
-rsp_b: dq 0
-no_word: db 'There is no such word', 10, 0
-
-; 1 for compil mode, 0 for interpret mode
-state: db 0
-here: dq dict
-
-program_stub: dq 0
-; two indirect level
+last_word: 	dq link
+rsp_b: 		dq 0
+no_word: 	db ' : There is no such word', 10, 0
+here: 		dq dict
+xt_exit: 	dq exit
+program_stub: 	dq 0
 xt_interpreter: dq .interpreter
-    .interpreter: dq interpreter_loop
-
+.interpreter: 	dq interpreter_loop
 
 section .text
 global _start
 _start:
 	mov rstack, rs + 65536 * 8
 	mov [rsp_b], rsp
-    mov pc, xt_interpreter
-    jmp next
+    	mov pc, xt_interpreter
+    	jmp next
 
 interpreter_loop:
-    mov al, byte [state]    ; check mode
-    test al, al
-    jnz compiler_loop
-
-	call read_word		    ; word <- stdin
+    	cmp qword [state], 0  		; check mode
+    	jne compiler_loop
 
 	mov rdi, word_buffer
-	call find_word		    ; try as defined word
-
+	call read_word		    	; word_buffer <- stdin			            		 
+	call find_word			; try as defined word
+					; rax <- w_word
 	test rax, rax
-	jz .parse_number	    ; if !(word in dict) -> try parse as number
+	jz .parse_number	    	; if !(word in dict) -> try parse as number
 
 	mov rdi, rax
 	call cfa		        ; else rax <- xt_word
 
 	mov [program_stub], rax
-	mov pc, program_stub	; ptr to next execution token = found xt_word
+	mov pc, program_stub		; ptr to next ex token = found xt_word
 	jmp next
 .parse_number:
 	mov rdi, word_buffer
@@ -58,83 +50,79 @@ interpreter_loop:
 	jz .no_word		        ; if lenght == 0 rip
 
 	push rax		        ; else push n
-	jmp interpreter_loop
+	mov pc, xt_interpreter
+	jmp next
 .no_word:
-	mov rdi, no_word
-	call print_string
-	jmp interpreter_loop
+	call print_no_word
+	mov pc, xt_interpreter
+	jmp next
+
 
 compiler_loop:
-    mov al, byte [state]        ; check mode
-    test al, al
-    jz interpreter_loop
+    	cmp qword [state], 1      	; check mode
+    	jne interpreter_loop
 
-    call read_word              ; word <- stdin
+    	mov rdi, word_buffer
+    	call read_word              	; word_buffer <- stdin
+    	call find_word              	; try as defined word 
+					; rax <- w_word
+	test rax, rax
+    	jz .parse_number	        ; if !(word in dict) -> try parse as number
 
-    mov rdi, word_buffer
-    call find_word              ; try as defined word
+    	mov rdi, rax
+    	call cfa		        ; else rax <- xt_word
 
-    test rax, rax
-	jz .parse_number	        ; if !(word in dict) -> try parse as number
-
-    mov rdi, rax
-	call cfa		            ; else rax <- xt_word
-
-	mov dil, byte [rax - 1]     ; check as immediate
-	cmp dil, 1
-	jz .immediate               ; if flag == 1 interpret
-
-	mov rdi, [here]
-	mov [rdi], rax              ; else add xt to here
-	add rdi, 8
-	mov [here], rdi
-
-	jmp compiler_loop
+    	cmp byte [rax - 1], 1		; check as immediate
+    	je .immediate               
+	
+    	mov rdi, [here]			; else add xt to here
+   	mov [rdi], rax              	
+   	add qword [here], 8
+    	jmp compiler_loop
 .parse_number:
-	mov rdi, word_buffer
-	call parse_int
+    	mov rdi, word_buffer
+    	call parse_int
 
-	test rdx, rdx
-	jz .no_word		            ; if lenght == 0 rip
+    	test rdx, rdx
+    	jz .no_word		        ; if lenght == 0 rip
 
 	mov rdi, [here]
-	mov qword [rdi], xt_lit     ; else add xt_lit
-	add rdi, 8
+    	cmp qword [rdi - 8], xt_branch	; check if prev == branch/branch0
+	je .branch
+	cmp qword [rdi - 8], xt_branch0
+	je .branch
 
-	mov [rdi], rax              ; add number
-	add rdi, 8
-	mov [here], rdi
-
+    	mov rdi, [here]			; else add xt_lit
+    	mov qword [rdi], xt_lit     	
+    	add qword [here], 8
+.branch:
+	mov rdi, [here]			; add num
+	mov [rdi], rax
+	add qword [here], 8
 	jmp compiler_loop
 .immediate:
-    mov w, rax
-    mov [program_stub], rax
-    mov pc, program_stub
-    jmp next
+    	mov [program_stub], rax
+    	mov pc, program_stub
+    	jmp next
 .no_word:
-	mov rdi, no_word
-	call print_string
-	jmp interpreter_loop
+	call print_no_word
+    	mov pc, xt_interpreter
+	jmp next
 
 next:
-	mov w, pc	; ptr to current xt_word
-	add pc, 8	; set pc as ptr to next xt (xt_interpreter / (xt_word or xt_exit) for colon)
-	mov w, [w]	; ptr to word_impl or docol
-	jmp [w]		; jmp to word_impl or docol
+    	mov w, pc		; ptr to current xt_word
+    	add pc, 8		; set pc as ptr to next xt (xt_interpreter / (xt_word or xt_exit) for colon)
+    	mov w, [w]		; ptr to word_impl or docol
+    	jmp [w]			; jmp to word_impl or docol
 
-; for colon-words
-section .data
-xt_exit: dq exit
-
-section .text
 docol:
-	sub rstack, 8	; rstack ptr to new value
-	mov [rstack], pc; push next xt
-	add w, 8	    ; set w as ptr to first xt_word of colon-word
-	mov pc, w	    ; pc = w
-	jmp next	    ; jmp to set pc, w and execute first xt_word
+	sub rstack, 8		
+	mov [rstack], pc	; save ptr to next xt
+	add w, 8	    	
+	mov pc, w	    	
+	jmp next	    	
 
 exit:
-	mov pc, [rstack]; restore ptr to next xt
-	add rstack, 8	; rstack ptr to last value
-	jmp next	    ; jmp to execution next xt
+	mov pc, [rstack]	; restore ptr to next xt
+	add rstack, 8		
+	jmp next	    	
